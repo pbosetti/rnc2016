7c947226445565ff2890197f91950a962b27c55d
Paolo Bosetti <paolo.bosetti@unitn.it>
2017-02-02 13:40:07 +0100

    Added example taken from exam of January 2017

---
+++ examples/exam1.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require "./lib/lathe.rb"
+
+cfg = {
+  unit_energy: 1.1, # W s / mm^3
+  speed: 600,       # mm/min
+  feed: 0.6         # mm/rev
+}
+
+# Instantiate Lathe and define parameters
+lathe  = Lathe.new(cfg)
+diam   = 150 # Workpiece diameter
+d0     = 2   # Initial depth of cut
+df     = 5   # Final depth of cut
+length = 250 # Workpiece length
+step   = 10  # Step length, in mm
+n      = (length / step).to_i # Total number of steps
+
+# Print table header, same column widths used later on
+header = %w(i x d mrr fr N T P)
+puts "%3s %9s %9s %9s %9s %9s %9s %9s" % header
+
+# Loop on z position
+0.upto(n) do |i|
+  z = step * i # Current z position
+  doc = d0 + (z * (df - d0)/length.to_f) # current depth of cut
+  d = diam - 2 * doc # current cutting diameter
+  
+  # Prepare data array, one per line
+  ary = [i, x, d]
+  ary += lathe.status(d, doc)
+  
+  # Four significant digits: we must use scientific notation
+  # '%.3e' means 'use scientific notation with 3 decimals'
+  puts "%3d %.3e %.3e %.3e %.3e %.3e %.3e %.3e" % ary
+end
---
+++ examples/lib/lathe.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+class Lathe
+  attr_accessor :unit_energy, # W s/mm^3 
+                :feed,        # feed per revolution, mm/rev
+                :speed        # cutting speed, mm^s
+  
+  def initialize(cfg)
+    @unit_energy = cfg[:unit_energy] || 0.0 # cfg field or a default value
+    @speed       = cfg[:speed]       || 0.0 # cfg field or a default value
+    @feed        = cfg[:feed]        || 0.0 # cfg field or a default value
+  end
+  
+  def rotary_speed(diam)
+    return (@speed / (Math::PI * diam)) # rpm
+  end
+  
+  def feed_rate(diam)
+    return self.rotary_speed(diam) * @feed
+  end
+  
+  def mrr(doc) # doc is depth of cut
+    return doc * @feed * @speed
+  end
+  
+  def torque(diam, doc) # T = P/omega
+    return self.power(doc) / (self.rotary_speed(diam) / 120.0 * Math::PI)
+  end
+  
+  def power(doc)
+    return self.mrr(doc) * @unit_energy
+  end
+  
+  def status(diam, doc)
+    return [self.mrr(doc), self.feed_rate(diam), self.rotary_speed(diam), self.torque(diam, doc), self.power(doc)]
+  end
+  
+end
